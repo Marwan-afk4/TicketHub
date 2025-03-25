82820aaff543170325794ed71fd137d196b90aa5
Marwan-afk4 <fk413691@gmail.com>
2025-03-25 22:08:33 +0200

    done

---
+++ app/Models/Aminity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aminity extends Model
+{
+
+
+    protected $fillable = [
+        'name',
+        'icon',
+        'status',
+    ];
+}
