6a1cffe9f7e656a4e4e83ee5408289f5f5779578
Ahmed <Wegostationdev@gmail.com>
2025-03-26 22:10:25 +0200

    commit

--- routes/agent.php
+++ routes/agent.php
@@ -5,12 +5,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\Agent\Car\CarController;
+use App\Http\Controllers\Api\Agent\Bus\BusController;
 
 Route::middleware(['auth:sanctum','IsAgent'])->group(function () {
     Route::controller(CarController::class)
     ->middleware(['can:isPrivate'])->prefix('car')->group(function(){
         Route::get('/', 'view');
         Route::get('/item/{id}', 'car');
+        Route::post('/add', 'create');
+        Route::post('/update/{id}', 'modify');
+        Route::delete('/delete/{id}', 'delete');
+    });
+
+    Route::controller(BusController::class)
+    ->middleware(['can:isBus'])->prefix('bus')->group(function(){
+        Route::get('/', 'view');
+        Route::get('/item/{id}', 'bus');
         Route::post('/add', 'create');
         Route::post('/update/{id}', 'modify');
         Route::delete('/delete/{id}', 'delete');
