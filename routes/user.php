bbd5caf976c5dc01cf23e09f59bf1906228d957a
Ahmed <Wegostationdev@gmail.com>
2025-03-26 12:06:17 +0200

    commit

--- routes/user.php
+++ routes/user.php
@@ -9,16 +9,12 @@ use App\Http\Controllers\Api\User\Profile\ProfileController;
 use App\Http\Controllers\Api\User\Wallet\WalletController;
 use App\Http\Controllers\Api\User\Points\PointsController;
 
-Route::controller(BookingController::class)->prefix('/booking')
-->group(function(){
-    Route::get('/lists', 'lists'); 
-    Route::post('/', 'filter'); 
-});
-
 Route::middleware(['auth:sanctum','IsUser'])->group(function () {
     Route::controller(BookingController::class)->prefix('/booking')
-    ->group(function(){ 
-        Route::get('/history', 'history'); 
+    ->group(function(){
+        Route::get('/lists', 'lists')->withOutMiddleware(['auth:sanctum', 'IsUser']);
+        Route::get('/history', 'history');
+        Route::post('/', 'filter')->withOutMiddleware(['auth:sanctum', 'IsUser']);
         Route::post('/payment', 'payment');
         Route::post('/payment_wallet', 'payment_wallet');
         Route::post('/private_request', 'private_request');
