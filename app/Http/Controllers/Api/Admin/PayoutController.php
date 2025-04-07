ca9677aadcdec7c597ef4e990c09b69bb5d52ea2
Marwan-afk4 <fk413691@gmail.com>
2025-04-07 17:17:55 +0200

    payoutt

--- app/Http/Controllers/Api/Admin/PayoutController.php
+++ app/Http/Controllers/Api/Admin/PayoutController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\PayoutRequest;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class PayoutController extends Controller
 {
 
+
+    public function getPayoutRequest(){
+        $payout = PayoutRequest::where('status', 'pending')
+        ->with(['currency:id,name,symbol','payment_method:id,name,image','agent:id,name,email,phone,image'])->get();
+        $data =[
+            'payout' => $payout
+        ];
+        return response()->json($data);
+    }
 
     public function cancelPayout(Request $request, $id)
     {
@@ -25,5 +35,26 @@ class PayoutController extends Controller
             'rejected_reason' => $request->rejected_reason
         ]);
         return response()->json(['message' => 'Payout request has been rejected'], 200);
+    }
+
+    public function confirmPayout(Request $request, $id){
+        $payout = PayoutRequest::find($id);
+        $agent_id = $payout->agent_id;
+        $wallet = Wallet::where('user_id', $agent_id)->where('currency_id', $payout->currency_id)->first();
+        // dd($wallet->amount);
+        if($wallet->amount < $payout->amount){
+            return response()->json(['message' => 'Insufficient balance'], 400);
+        }
+        elseif($wallet->amount == $payout->amount || $wallet->amount > $payout->amount){
+            $wallet->update([
+                'amount' => $wallet->amount - $payout->amount,
+                'total' => $wallet->total - $payout->amount
+            ]);
+        }
+        $payout->update([
+            'status' => 'approved'
+        ]);
+        return response()->json(['message' => 'Payout request has been confirmed'], 200);
+
     }
 }
