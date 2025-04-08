4873cd25e8be95d1ff52ee1f53843cbf1d213341
Ahmed <Wegostationdev@gmail.com>
2025-04-08 16:55:20 +0200

    commit

--- app/Http/Controllers/Api/Agent/Booking/BookingController.php
+++ app/Http/Controllers/Api/Agent/Booking/BookingController.php
@@ -31,6 +31,7 @@ class BookingController extends Controller
         ->where('status', 'confirmed')
         ->get()
         ->map(function($item){
+            $item->booking = $item?->booking?->id ?? null;
             $item->trip_type = $item?->trip?->trip_type ?? null;
             $item->travel_status = $item?->booking?->status ?? null;
             $item->operator = $item->total - $item->commission;
