730a64ed2b0a16cee61dda3cb64d0e95400cb9bd
Marwan-afk4 <fk413691@gmail.com>
2025-04-13 17:25:09 +0200

    aaa

--- app/Http/Requests/TripRequest.php
+++ app/Http/Requests/TripRequest.php
@@ -55,7 +55,7 @@ class TripRequest extends FormRequest
                 'day' => 'sometimes|array',
                 'day.*' => 'required_with:day|string|in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
                 'start_date' => 'nullable|date',
-                'train_id' => 'sometimes|exists:trains,id',
+                'train_id' => 'nullable|exists:trains,id',
             ];
         }
 
