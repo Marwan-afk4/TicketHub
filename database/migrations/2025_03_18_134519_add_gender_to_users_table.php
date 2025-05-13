56304ab2b469abafd6e910d3f2815a6df7f2aa72
Ahmed <Wegostationdev@gmail.com>
2025-05-13 16:05:21 +0200

    commit

--- database/migrations/2025_03_18_134519_add_gender_to_users_table.php
+++ database/migrations/2025_03_18_134519_add_gender_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('gender',['male','female']);
+            $table->enum('gender',['male','female', 'other']);
             $table->foreignId('nationality_id')->nullable()->constrained('nationalities')->onDelete('set null');
         });
     }
